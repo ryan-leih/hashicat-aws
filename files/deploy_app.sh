731d5a62bbd810fd0c87963db4ca2b093a413d62
ryan-leih <63402999+ryan-leih@users.noreply.github.com>
2020-04-09 14:09:40 +0200

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to Boris Johnson's app. Replace this text with your own.
+  Welcome to Boris Johnson's app. Boris Johnson does not approve.
   <!-- END -->
 
   </div>
